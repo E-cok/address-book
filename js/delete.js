fc23c15ae730bc66a3c1eed9b5cfa56e7362acc8
Cat-Hanbit <hello.git.whdgus5566@naver.com>
2020-06-21 10:53:35 +0900

    id 값 추가

--- js/delete.js
+++ js/delete.js
@@ -1,15 +1,15 @@
-function data_delete(name) {
+function data_delete(id) {
     //삭제할 데이터를 검색합니다.
-    var db = openDatabase('table', '1.0', 'test', 5 * 1024 * 1024);
+    var db = openDatabase('table', '1.0', 'test', 6 * 1024 * 1024);
     //데이터베이스를 생성합니다.
     db.transaction(function (tx) {
         //테이블의 모든 값을 result에 리스트로 불러옵니다.
         tx.executeSql("select * from log", [], function (tx, result) {
             //리스트를 전부 읽어서 값이 존재하는지 확인합니다.
             for (var i = 0; i < result.rows.length; i++) {
-                if (result.rows.item(i).name == name) {
+                if (result.rows.item(i).id == id) {
                     //삭제할 데이터를 찾은경우 함수 호출
-                    delete_(name);
+                    delete_(id);
                     return;
                 } if (i + 1 == result.rows.length) {
                     //삭제할 데이터가 존재하지 않는경우
@@ -20,12 +20,12 @@ function data_delete(name) {
     });
 }
 
-function delete_(name) {
+function delete_(id) {
     //테이블에서 바로 삭제합니다.
-    var db = openDatabase('table', '1.0', 'test', 5 * 1024 * 1024);
+    var db = openDatabase('table', '1.0', 'test', 6 * 1024 * 1024);
 
     db.transaction(function (tx) {
-        tx.executeSql('DELETE FROM log WHERE name=?', [name]);
+        tx.executeSql('DELETE FROM log WHERE id=?', [id]);
 
     });
 }
--- js/search_data.js
+++ js/search_data.js
@@ -1,6 +1,6 @@
 function data_list() {
     //데이터베이스에서 데이터를 가져오는 스크립트입니다.
-    var db = openDatabase('table', '1.0', 'Test', 5 * 1024 * 1024);
+    var db = openDatabase('table', '1.0', 'Test', 6 * 1024 * 1024);
     var data_list;
 
     db.transaction(function (tx) {
@@ -9,11 +9,12 @@ function data_list() {
             var len = result.rows.length, i;
 
             for (i = 0; i < len; i++) {
-                data_list[0][i] = result.rows.item(i).name;
-                data_list[1][i] = result.rows.item(i).job;
-                data_list[2][i] = result.rows.item(i).email;
-                data_list[3][i] = result.rows.item(i).call;
-                data_list[4][i] = result.rows.item(i).memo;
+                data_list[0][i] = result.rows.item(i).id;
+                data_list[1][i] = result.rows.item(i).name;
+                data_list[2][i] = result.rows.item(i).job;
+                data_list[3][i] = result.rows.item(i).email;
+                data_list[4][i] = result.rows.item(i).call;
+                data_list[5][i] = result.rows.item(i).memo;
             }
             return data_list;
         }, null);
