d1c93d3dbe25ea9babf921535598a2a35ec6498f
E-dok <goqwe1250@gmail.com>
2020-06-28 22:12:30 +0900

    전화번호 검색 기능 css 적용 및 테스트 통과

--- js/main.js
+++ js/main.js
@@ -49,3 +49,40 @@ function only_my_acc() {
 		});
 	});
 }
+
+
+function search() {
+	var table = document.getElementById("data_table");
+
+	$('#data_table').empty();
+
+	var db = openDatabase('listtable', '1.0', 'data_list', 2 * 1024 * 1024);
+
+	db.transaction(function (tx) {
+		tx.executeSql("select * from listdata", [], function (tx, result) {
+			var len = result.rows.length, i;
+
+			if (document.getElementById("searching").value == "") {
+				alert("연락처를 입력해주세요");
+				reload();
+			} else {
+				for (i = 0; i < len; i++) {
+					if (result.rows.item(i).call == document.getElementById("searching").value) {
+						var row = table.insertRow(table.rows.length);
+						row.insertCell(0).innerHTML = result.rows.item(i).name;
+						row.insertCell(1).innerHTML = result.rows.item(i).call;
+						row.insertCell(2).innerHTML = result.rows.item(i).job;
+						row.insertCell(3).innerHTML = result.rows.item(i).email;
+						row.insertCell(4).innerHTML = result.rows.item(i).memo;
+					}
+				}
+			}
+
+
+		});
+	});
+}
+
+function auto_add_dash() {
+    $(document).on("keyup", "#searching", function() { $(this).val( $(this).val().replace(/[^0-9]/g, "").replace(/(^02|^0505|^1[0-9]{3}|^0[0-9]{2})([0-9]+)?([0-9]{4})$/,"$1-$2-$3").replace("--", "-") ); });
+}
