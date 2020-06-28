ca7526807a540b77af0c5acb4c374eb16f10589d
Cat-Hanbit <hello.git.whdgus5566@naver.com>
2020-06-28 18:34:59 +0900

    자잘한 오류 수정

--- js/data_input.js
+++ js/data_input.js
@@ -1,37 +1,56 @@
 function data_input() {
+    alert("진입성공")
     var id = window.localStorage.getItem("id");
     var name = document.getElementsByClassName("write")[0].value;
     var call = document.getElementsByClassName("write")[1].value;
     var job = document.getElementsByClassName("write")[2].value;
     var email = document.getElementsByClassName("write")[3].value;
     var memo = document.getElementsByClassName("write")[4].value;
+    alert(id)
+    alert(name)
+    alert(call)
+    alert(job)
+    alert(email)
 
     var db = openDatabase('listtable', '1.0', 'data_list', 2 * 1024 * 1024);
-
+    alert("db오픈")
     db.transaction(function (tx) {
-
+        alert("db접속")
+        tx.executeSql('CREATE TABLE IF NOT EXISTS listdata (id, name, call, job, email, memo)');
         tx.executeSql("select * from listdata", [], function (tx, result) {
-            for (var i = 0; i < result.rows.length; i++) {
-                if (result.rows.item(i).call == call) {
-                    alert("이미 존재하는 전화번호입니다.");
-                    break;
-                }
-                if (i + 1 == result.rows.length) {
-                    input();
+            alert(result.rows.length);
+            if (result.rows.length == 0) {
+                var db = openDatabase('listtable', '1.0', 'data_list', 2 * 1024 * 1024);
+
+                db.transaction(function (tx) {
+                    tx.executeSql('CREATE TABLE IF NOT EXISTS listdata (id, name, call, job, email, memo)');
+                    tx.executeSql('INSERT INTO listdata (id, name, call, job, email, memo) VALUES(?, ?, ?, ?, ?, ?)', [id, name, call, job, email, memo]);
+                    alert("등록되었습니다.");
                     self.close();
-                    return;
+                });
+
+            } else {
+                for (var i = 0; i < result.rows.length; i++) {
+                    if (result.rows.item(i).call == call) {
+                        alert("이미 존재하는 전화번호입니다.");
+                        break;
+                    }
+
+                    if (i + 1 == result.rows.length) {
+                        var db = openDatabase('listtable', '1.0', 'data_list', 2 * 1024 * 1024);
+
+                        db.transaction(function (tx) {
+                            tx.executeSql('CREATE TABLE IF NOT EXISTS listdata (id, name, call, job, email, memo)');
+                            tx.executeSql('INSERT INTO listdata (id, name, call, job, email, memo) VALUES(?, ?, ?, ?, ?, ?)', [id, name, call, job, email, memo]);
+                            alert("등록되었습니다.");
+                            self.close();
+                        });
+                        return;
+                    }
                 }
             }
+
         });
-        function input() {
 
-            var db = openDatabase('listtable', '1.0', 'data_list', 2 * 1024 * 1024);
-
-            db.transaction(function (tx) {
-                tx.executeSql('CREATE TABLE IF NOT EXISTS listdata (id, name, call, job, email, memo)');
-                tx.executeSql('INSERT INTO listdata (id, name, call, job, email, memo) VALUES(?, ?, ?, ?, ?, ?)', [id, name, call, job, email, memo]);
-                alert("등록되었습니다.");
-            });
-        }
     });
 }
--- js/finding_script.js
+++ js/finding_script.js
@@ -18,7 +18,7 @@ function find_with_name() {
                         window.close();
                         return;
                     }
-                }else if (len == i+1){
+                } else if (len == i + 1) {
                     alert("존재하지 않는 이름입니다.");
                 }
             }
@@ -28,27 +28,26 @@ function find_with_name() {
 
 function find_with_call() {
     var db = openDatabase('account1', '1.0', 'test', 2 * 1024 * 1024);
-    var call = document.getElementsByClassName("write")[0].value;
-    var id = document.getElementsByClassName("write")[1].value;
+    var tel = document.getElementsByClassName("write")[2].value;
+    var id = document.getElementsByClassName("write")[3].value;
+
     db.transaction(function (tx) {
         tx.executeSql("select * from list", [], function (tx, result) {
 
             var len = result.rows.length, i;
 
-            for (i = 0; i < len; i++) {
-                if (result.rows.item(i).call == call) {
-                    if (result.rows.item(i).id == id) {
+            for (i = 0; i < result.rows.length; i++) {
+                if (result.rows.item(i).id == id) {
+                    if (result.rows.item(i).tel == tel) {
                         alert("비밀번호는(" + result.rows.item(i).password1 + ")입니다.");
                         window.close();
                         return;
                     } else {
-                        alert('일치하지 않습니다.');
-                        window.close();
-                        return;
+                        alert('두 정보가 일치하지 않습니다.');
                     }
-                }else if (len == i+1){
-                    alert("존재하지 않는 이름입니다.");
                 }
+            } if (i + 1 == len){
+                alert("아이디를 찾지 못했습니다.");
             }
         });
     });
@@ -56,27 +55,25 @@ function find_with_call() {
 
 function find_with_birth() {
     var db = openDatabase('account1', '1.0', 'test', 2 * 1024 * 1024);
-    var birth = document.getElementsByClassName("write")[0].value;
-    var id = document.getElementsByClassName("write")[1].value;
+    var birth = document.getElementsByClassName("write")[4].value;
+    var id = document.getElementsByClassName("write")[5].value;
     db.transaction(function (tx) {
         tx.executeSql("select * from list", [], function (tx, result) {
 
             var len = result.rows.length, i;
 
             for (i = 0; i < len; i++) {
-                if (result.rows.item(i).birth == birth) {
-                    if (result.rows.item(i).id == id) {
+                if (result.rows.item(i).id == id) {
+                    if (result.rows.item(i).birth == birth) {
                         alert("비밀번호는(" + result.rows.item(i).password1 + ")입니다.");
                         window.close();
                         return;
                     } else {
                         alert('일치하지 않습니다.');
-                        window.close();
-                        return;
                     }
-                }else if (len == i+1){
-                    alert("존재하지 않는 이름입니다.");
                 }
+            } if(i + 1 == len){
+                alert("존재하지 않는 아이디입니다.");
             }
         });
     });
